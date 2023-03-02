66133d7854717123843f05c9bcad25fa77a78f05
pindukurisairam <sairamsai524@gmail.com>
2023-03-02 07:39:11 +0000

    solution

---
+++ calculator.js
@@ -0,0 +1,10 @@
+const add = (a, b, c) => {
+  return a + b + c;
+};
+
+const sub = (a, b, c) => {
+  return a - b - c;
+};
+
+exports.add = add;
+exports.sub = sub;
---
+++ index.js
@@ -0,0 +1,4 @@
+const { add, sub } = require("./calculator");
+
+console.log(add(6, 3, 9));
+console.log(sub(6, 5, 9));
